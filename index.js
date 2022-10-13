bba023b54e6eb6edee602003eacb2a740b3adf7e
john.adeba <John.Adeba@endava.com>
2022-10-13 10:57:14 +0200

    procfile added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/frontend/browser")));
 
 app.use("/api", router);
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
 	res.sendFile(path.join(__dirname + "/frontend/browser/index.html"));
 });
 
